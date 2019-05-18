620ea8e777e1bf12efbcc57087cfc40dfcd109c6
natans <natans@wix.com>
2019-05-18 09:10:52 +0300

    now including the sample test

---
+++ src/java/com/wix/sample/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.wix.sample;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleTest() {
+        assertTrue("assert true is true", false);
+    }
+}
